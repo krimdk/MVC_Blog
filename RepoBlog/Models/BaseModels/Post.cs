fc6087855a33d5ecf55c9e73261cd20e568da650
Kim Lind Jensen <kim@joh.dk>
2015-12-01 09:32:20 +0100

    oprettet

---
+++ RepoBlog/Models/BaseModels/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoBlog.Models.BaseModels
+{
+    public class Post
+    {
+        private int ID { get; set; }
+        private string Overskrift { get; set; }
+        private string Tekst { get; set; }
+        private DateTime Dato { get; set; }
+        private string Forfatter { get; set; }
+    }
+}
