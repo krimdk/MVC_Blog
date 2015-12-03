96eb050518ae14e5e87305d5f3b0322295242361
Kim Lind Jensen <kim@joh.dk>
2015-12-03 09:03:35 +0100

    onsdag arbejde

--- MVC_Blog/Controllers/BlogController.cs
+++ MVC_Blog/Controllers/BlogController.cs
@@ -22,14 +22,49 @@ namespace MVC_Blog.Controllers
         {
             return View(postFac.Get(id));
         }
+        public ActionResult Delete(int id)
+        {
+            postFac.Delete(id);
+            return View("Index", postFac.GetIndexData());
+        }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Edit(Post p)
         {
             p.Dato = DateTime.Now;
-            postFac.Update(p);
 
+            if (ModelState.IsValid)
+            {
+                postFac.Update(p);
+            }
+            
             return View(postFac.Get(p.ID));
+        }
+
+        public ActionResult Creat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Creat(Post p)
+        {
+            p.Dato = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                postFac.Insert(p);
+
+                ViewBag.msg = "Posten er oprettet";
+            }
+            else
+            {
+                ViewBag.msg = "Der opstod en fejl, posten er ikke oprettet";
+            }
+            
+            return View();
         }
     }
 }
--- RepoBlog/Models/BaseModels/Post.cs
+++ RepoBlog/Models/BaseModels/Post.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace RepoBlog.Models.BaseModels
 {
     public class Post
     {
         public int ID { get; set; }
+        [Required]
         public string Overskrift { get; set; }
+        [Required]
         public string Tekst { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Dato { get; set; }
+        [Required]
         public string Forfatter { get; set; }
     }
 }
